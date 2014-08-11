20949d50912804b3589be58b1b3e16cfa750bf59
Ryan Mayeda <ryan@shotgunsoftware.com>
2014-08-10 21:05:51 -0700

    Adding support for Motion Builder
    There was no scene operation hook for Motion Builder yet, added it
    based on the hook for tk-multi-workfiles

---
+++ hooks/scene_operation_tk-motionbuilder.py
@@ -0,0 +1,73 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+from pyfbsdk import FBApplication
+
+import tank
+from tank import Hook
+from tank import TankError
+from tank.platform.qt import QtGui
+
+class SceneOperation(Hook):
+    """
+    Hook called to perform an operation with the
+    current scene
+    """
+
+    def execute(self, operation, file_path, context, parent_action, file_version, read_only, **kwargs):
+        """
+        Main hook entry point
+        
+        :param operation:       String
+                                Scene operation to perform
+        
+        :param file_path:       String
+                                File path to use if the operation
+                                requires it (e.g. open)
+                    
+        :param context:         Context
+                                The context the file operation is being
+                                performed in.
+                    
+        :param parent_action:   This is the action that this scene operation is
+                                being executed for.  This can be one of:
+                                - open_file
+                                - new_file
+                                - save_file_as 
+                                - version_up
+                        
+        :param file_version:    The version/revision of the file to be opened.  If this is 'None'
+                                then the latest version should be opened.
+        
+        :param read_only:       Specifies if the file should be opened read-only or not
+                            
+        :returns:               Depends on operation:
+                                'current_path' - Return the current scene
+                                                 file path as a String
+                                'reset'        - True if scene was reset to an empty 
+                                                 state, otherwise False
+                                all others     - None
+        """
+        
+        fb_app = FBApplication()
+
+        if operation == "current_path":
+            # return the current scene path
+            return fb_app.FBXFileName
+        elif operation == "open":
+            # do new scene as Maya doesn't like opening
+            # the scene it currently has open!
+            fb_app.FileOpen( file_path )
+        elif operation == "save":
+            # save the current scene:
+            # Note - have to pass the current scene name to
+            # avoid showing the save-as dialog
+            fb_app.FileSave(fb_app.FBXFileName)
