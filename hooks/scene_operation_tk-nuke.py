27fc9d799d79d9a1be78dd76e19e97ffc08bc977
Jeff Beeland <jeff.beeland@autodesk.com>
2015-10-22 14:22:54 -0700

    Merges in Hiero functionality into tk-nuke scene operation hook.

--- hooks/scene_operation_tk-nuke.py
+++ hooks/scene_operation_tk-nuke.py
@@ -20,8 +20,7 @@ class SceneOperation(Hook):
     Hook called to perform an operation with the 
     current scene
     """
-    
-    def execute(self, operation, file_path, **kwargs):
+    def execute(self, *args, **kwargs):
         """
         Main hook entry point
         
@@ -37,6 +36,35 @@ class SceneOperation(Hook):
                                      file path as a String
                     all others     - None
         """
+        engine = self.parent.engine
+        if hasattr(engine, "hiero_enabled") and engine.hiero_enabled:
+            return self._hiero_execute(*args, **kwargs)
+        else:
+            return self._nuke_execute(*args, **kwargs)
+
+    def _hiero_execute(self, operation, file_path, **kwargs):
+        import hiero.core
+        if operation == "current_path":
+            # return the current script path
+            project = self._get_current_project()
+            curr_path = project.path().replace("/", os.path.sep)
+            return curr_path
+        
+        elif operation == "open":
+            # first close the current project then open the specified file 
+            project = self._get_current_project()
+            project.close()
+            # manually fire signal since Hiero doesn't fire this when loading 
+            # from the tk file manager
+            hiero.core.events.sendEvent("kBeforeProjectLoad", None)
+            hiero.core.openProject(file_path.replace(os.path.sep, "/"))
+
+        elif operation == "save":
+            # save the current script:
+            project = self._get_current_project()
+            project.save()        
+    
+    def _nuke_execute(self, operation, file_path, **kwargs):
         if file_path:
             file_path = file_path.replace("/", os.path.sep)        
         
@@ -52,3 +80,24 @@ class SceneOperation(Hook):
         elif operation == "save":
             # save the current script:
             nuke.scriptSave()
+
+    def _get_current_project(self):
+        """
+        Returns the current project based on where in the UI the user clicked 
+        """
+        import hiero.core
+        # get the menu selection from hiero engine
+        selection = self.parent.engine.get_menu_selection()
+
+        if len(selection) != 1:
+            raise TankError("Please select a single Project!")
+        
+        if not isinstance(selection[0] , hiero.core.Bin):
+            raise TankError("Please select a Hiero Project!")
+            
+        project = selection[0].project()
+        if project is None:
+            # apparently bins can be without projects (child bins I think)
+            raise TankError("Please select a Hiero Project!")
+        
+        return project
